9e7e7d8dd1221bde7faa2d501455f0a64e82eb4b
Ron Badur <ronbadur02@gmail.com>
2019-01-27 21:15:06 +0200

    intial commit

---
+++ src/main/java/algorithm/GeneticAlgorithm.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+import entities.Shift;
+import entities.Worker;
+
+import java.util.List;
+
+public class GeneticAlgorithm {
+
+    public void scheduleShifts(List<Worker> workers, List<Shift> shifts) {
+
+        
+    }
+
+}
---
+++ src/main/java/solution_scorer/SolutionScorer.java
@@ -0,0 +1,4 @@
+package solution_scorer;
+
+public class SolutionScorer {
+}
