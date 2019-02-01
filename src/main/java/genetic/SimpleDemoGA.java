0d4c26702be4f109a544a134541fb8bf41a2c258
Roee Rokah <roee.rokah@gmail.com>
2019-02-01 19:24:24 +0200

    feat(Genetic Algorithm): generated individual that is specific for our problem

--- src/main/java/genetic/SimpleDemoGA.java
+++ src/main/java/genetic/SimpleDemoGA.java
@@ -25,40 +25,40 @@ public class SimpleDemoGA {
         demo.population.initializePopulation(10);
 
         //Calculate fitness of each individual
-        demo.population.calculateFitness();
-
-        System.out.println("Generation: " + demo.generationCount + " Fittest: " + demo.population.fittest);
-
-        //While population gets an individual with maximum fitness
-        while (demo.population.fittest < 5) {
-            ++demo.generationCount;
-
-            //Do selection
-            demo.selection();
-
-            //Do crossover
-            demo.crossover();
-
-            //Do mutation under a random probability
-            if (rn.nextInt()%7 < 5) {
-                demo.mutation();
-            }
-
-            //Add fittest offspring to population
-            demo.addFittestOffspring();
-
-            //Calculate new fitness value
-            demo.population.calculateFitness();
-
-            System.out.println("Generation: " + demo.generationCount + " Fittest: " + demo.population.fittest);
-        }
-
-        System.out.println("\nSolution found in generation " + demo.generationCount);
-        System.out.println("Fitness: "+demo.population.getFittest().fitness);
-        System.out.print("Genes: \n");
-        for (int i = 0; i < 5; i++) {
-            System.out.print(demo.population.getFittest().genes[i]);
-        }
+//        demo.population.calculateFitness();
+//
+//        System.out.println("Generation: " + demo.generationCount + " Fittest: " + demo.population.fittest);
+//
+//        //While population gets an individual with maximum fitness
+//        while (demo.population.fittest < 5) {
+//            ++demo.generationCount;
+//
+//            //Do selection
+//            demo.selection();
+//
+//            //Do crossover
+//            demo.crossover();
+//
+//            //Do mutation under a random probability
+//            if (rn.nextInt()%7 < 5) {
+//                demo.mutation();
+//            }
+//
+//            //Add fittest offspring to population
+//            demo.addFittestOffspring();
+//
+//            //Calculate new fitness value
+//            demo.population.calculateFitness();
+//
+//            System.out.println("Generation: " + demo.generationCount + " Fittest: " + demo.population.fittest);
+//        }
+//
+//        System.out.println("\nSolution found in generation " + demo.generationCount);
+//        System.out.println("Fitness: "+demo.population.getFittest().fitness);
+//        System.out.print("Genes: \n");
+//        for (int i = 0; i < 5; i++) {
+//            System.out.print(demo.population.getFittest().genes[i]);
+//        }
 
     }
 
@@ -74,42 +74,42 @@ public class SimpleDemoGA {
 
     //Crossover
     private void crossover() {
-        Random rn = new Random();
-
-        //Select a random crossover point
-        int crossOverPoint = rn.nextInt(population.individuals[0].geneLength);
-
-        //Swap values among parents
-        for (int i = 0; i < crossOverPoint; i++) {
-            int temp = fittest.genes[i];
-            fittest.genes[i] = secondFittest.genes[i];
-            secondFittest.genes[i] = temp;
-
-        }
+//        Random rn = new Random();
+//
+//        //Select a random crossover point
+//        int crossOverPoint = rn.nextInt(population.individuals[0].geneLength);
+//
+//        //Swap values among parents
+//        for (int i = 0; i < crossOverPoint; i++) {
+//            int temp = fittest.genes[i];
+//            fittest.genes[i] = secondFittest.genes[i];
+//            secondFittest.genes[i] = temp;
+//
+//        }
 
     }
 
     //Mutation
     private void mutation() {
-        Random rn = new Random();
-
-        //Select a random mutation point
-        int mutationPoint = rn.nextInt(population.individuals[0].geneLength);
-
-        //Flip values at the mutation point
-        if (fittest.genes[mutationPoint] == 0) {
-            fittest.genes[mutationPoint] = 1;
-        } else {
-            fittest.genes[mutationPoint] = 0;
-        }
-
-        mutationPoint = rn.nextInt(population.individuals[0].geneLength);
-
-        if (secondFittest.genes[mutationPoint] == 0) {
-            secondFittest.genes[mutationPoint] = 1;
-        } else {
-            secondFittest.genes[mutationPoint] = 0;
-        }
+//        Random rn = new Random();
+//
+//        //Select a random mutation point
+//        int mutationPoint = rn.nextInt(population.individuals[0].geneLength);
+//
+//        //Flip values at the mutation point
+//        if (fittest.genes[mutationPoint] == 0) {
+//            fittest.genes[mutationPoint] = 1;
+//        } else {
+//            fittest.genes[mutationPoint] = 0;
+//        }
+//
+//        mutationPoint = rn.nextInt(population.individuals[0].geneLength);
+//
+//        if (secondFittest.genes[mutationPoint] == 0) {
+//            secondFittest.genes[mutationPoint] = 1;
+//        } else {
+//            secondFittest.genes[mutationPoint] = 0;
+//        }
     }
 
     //Get fittest offspring
@@ -141,30 +141,56 @@ public class SimpleDemoGA {
 //Individual class
 class Individual {
 
+    static final private int NUM_OF_WORKERS = 3;
+    static final private int NUM_OF_DAYS = 2;
+    static final private int NUM_OF_SHIFTS = 4;
+
     int fitness;
-    int[] genes = new int[5];
+    int[][][] genes = new int[NUM_OF_WORKERS][NUM_OF_DAYS][NUM_OF_SHIFTS];
     int geneLength = 5;
 
     public Individual() {
         Random rn = new Random();
 
         //Set genes randomly for each individual
-        for (int i = 0; i < genes.length; i++) {
-            genes[i] = Math.abs(rn.nextInt() % 2);
+        for (int day = 0; day < NUM_OF_DAYS; day++) {
+            for (int shift = 0; shift < NUM_OF_SHIFTS; shift++) {
+                // who of the workers will get the shift
+                int worker = Math.abs(rn.nextInt() % NUM_OF_WORKERS);
+                genes[worker][day][shift] = 1;
+            }
         }
+        printMatrix(genes);
 
         fitness = 0;
     }
-
-    //Calculate fitness
-    public void calcFitness() {
-
-        fitness = 0;
-        for (int i = 0; i < 5; i++) {
-            if (genes[i] == 1) {
-                ++fitness;
+    private void printMatrix(int[][][] matrix) {
+        System.out.println("------------------");
+        for (int worker = 0; worker < matrix.length; worker++) {
+            printf("man " + worker + ":");
+            for(int shift = 0; shift < matrix[worker][0].length; shift++) {
+                System.out.printf("shift " + shift + "|");
+            }
+            System.out.println();
+            for (int day = 0; day < matrix[worker].length; day++) {
+                System.out.printf("day " + day +";");
+                for(int shift = 0; shift < matrix[worker][day].length; shift++) {
+                    System.out.printf("%4d", matrix[worker][day][shift]);
+                    System.out.printf("    ");
+                }
+                System.out.println("");
             }
         }
+    }
+    //Calculate fitness
+    public void calcFitness() {
+//
+//        fitness = 0;
+//        for (int i = 0; i < 5; i++) {
+//            if (genes[i] == 1) {
+//                ++fitness;
+//            }
+//        }
     }
 
 }
