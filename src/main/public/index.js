1dd35272780dbdfc92383bfcc0f1cd8be65c2d31
Yarin Ronel <y3arin@gmail.com>
2019-02-18 23:10:36 +0200

    feat: Map request data to relevant graph data

--- src/main/public/index.js
+++ src/main/public/index.js
@@ -1,81 +1,195 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-	width = 960 - margin.left - margin.right,
-	height = 500 - margin.top - margin.bottom;
+// const margin = { top: 20, right: 20, bottom: 30, left: 50 },
+//   width = 960 - margin.left - margin.right,
+//   height = 500 - margin.top - margin.bottom;
 
-// parse the date / time
-var parseTime = d3.timeParse('%d-%b-%y');
+// // set the ranges
+// var x = d3.scaleLinear().range([0, width]);
+// var y = d3.scaleLinear().range([height, 0]);
 
-// set the ranges
-var x = d3.scaleTime().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
+// // define the 1st line
+// const dynamic = d3
+//   .line()
+//   .x(function(d) {
+//     return x(d.close);
+//   })
+//   .y(function(d) {
+//     return y(d.close);
+//   });
 
-// define the 1st line
-var valueline = d3.line()
-	.x(function (d) {
-		return x(d.date);
-	})
-	.y(function (d) {
-		return y(d.close);
-	});
+// // define the 2nd line
+// const genetic = d3
+//   .line()
+//   .x(function(d) {
+//     return x(d.open);
+//   })
+//   .y(function(d) {
+//     return y(d.open);
+//   });
 
-// define the 2nd line
-var valueline2 = d3.line()
-	.x(function (d) {
-		return x(d.date);
-	})
-	.y(function (d) {
-		return y(d.open);
-	});
+// // append the svg obgect to the body of the page
+// // appends a 'group' element to 'svg'
+// // moves the 'group' element to the top left margin
+// var svg = d3
+//   .select("body")
+//   .append("svg")
+//   .attr("width", width + margin.left + margin.right)
+//   .attr("height", height + margin.top + margin.bottom)
+//   .append("g")
+//   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select('body').append('svg')
-	.attr('width', width + margin.left + margin.right)
-	.attr('height', height + margin.top + margin.bottom)
-	.append('g')
-	.attr('transform',
-		'translate(' + margin.left + ',' + margin.top + ')');
+// // Get the data
+// d3.csv("data2.csv", function(error, data) {
+//   if (error) throw error;
 
-// Get the data
-d3.csv('data2.csv', function (error, data) {
-	if (error) throw error;
+//   // format the data
+//   data.forEach(function(d) {
+//     d.date = parseTime(d.date);
+//     d.close = +d.close;
+//     d.open = +d.open;
+//   });
 
-	// format the data
-	data.forEach(function (d) {
-		d.date = parseTime(d.date);
-		d.close = +d.close;
-		d.open = +d.open;
-	});
+//   // Scale the range of the data
+//   x.domain(
+//     d3.extent(data, function(d) {
+//       return d.date;
+//     })
+//   );
+//   y.domain([
+//     0,
+//     d3.max(data, function(d) {
+//       return Math.max(d.close, d.open);
+//     })
+//   ]);
 
-	// Scale the range of the data
-	x.domain(d3.extent(data, function (d) {
-		return d.date;
-	}));
-	y.domain([0, d3.max(data, function (d) {
-		return Math.max(d.close, d.open);
-	})]);
+//   // Add the valueline path.
+//   svg
+//     .append("path")
+//     .data([data])
+//     .attr("class", "line")
+//     .attr("d", valueline);
 
-	// Add the valueline path.
-	svg.append('path')
-		.data([data])
-		.attr('class', 'line')
-		.attr('d', valueline);
+//   // Add the valueline2 path.
+//   svg
+//     .append("path")
+//     .data([data])
+//     .attr("class", "line")
+//     .style("stroke", "red")
+//     .attr("d", valueline2);
 
-	// Add the valueline2 path.
-	svg.append('path')
-		.data([data])
-		.attr('class', 'line')
-		.style('stroke', 'red')
-		.attr('d', valueline2);
+//   // Add the X Axis
+//   svg
+//     .append("g")
+//     .attr("transform", "translate(0," + height + ")")
+//     .call(d3.axisBottom(x));
 
-	// Add the X Axis
-	svg.append('g')
-		.attr('transform', 'translate(0,' + height + ')')
-		.call(d3.axisBottom(x));
+//   // Add the Y Axis
+//   svg.append("g").call(d3.axisLeft(y));
+// });
 
-	// Add the Y Axis
-	svg.append('g')
-		.call(d3.axisLeft(y));
+const numberOfWorkers = 4;
+const requestURL = `http://localhost:8080/runStatistics?numberOfWorkers=${numberOfWorkers}`;
 
+// Figure out how to deal with CORS later on.
+// d3.json(requestURL, response => {
+//   console.log(response);
+// });
+
+d3.json("mock.json", (error, data) => {
+  if (error) throw error;
+
+  const workersScoreData = data.map((element, index) => ({
+    workers: index + 1,
+    dynamic: element.dynamicScore,
+    genetic: element.geneticScore
+  }));
+
+  const workersDurationData = data.map((element, index) => ({
+    workers: index + 1,
+    dynamic: element.dynamicDuration,
+    genetic: element.geneticDuration
+  }));
+
+  console.log(workersScoreData, workersDurationData);
 });
+
+// var margin = {top: 20, right: 20, bottom: 30, left: 50},
+// 	width = 960 - margin.left - margin.right,
+// 	height = 500 - margin.top - margin.bottom;
+
+// // parse the date / time
+// var parseTime = d3.timeParse('%d-%b-%y');
+
+// // set the ranges
+// var x = d3.scaleTime().range([0, width]);
+// var y = d3.scaleLinear().range([height, 0]);
+
+// // define the 1st line
+// var valueline = d3.line()
+// 	.x(function (d) {
+// 		return x(d.date);
+// 	})
+// 	.y(function (d) {
+// 		return y(d.close);
+// 	});
+
+// // define the 2nd line
+// var valueline2 = d3.line()
+// 	.x(function (d) {
+// 		return x(d.date);
+// 	})
+// 	.y(function (d) {
+// 		return y(d.open);
+// 	});
+
+// // append the svg obgect to the body of the page
+// // appends a 'group' element to 'svg'
+// // moves the 'group' element to the top left margin
+// var svg = d3.select('body').append('svg')
+// 	.attr('width', width + margin.left + margin.right)
+// 	.attr('height', height + margin.top + margin.bottom)
+// 	.append('g')
+// 	.attr('transform',
+// 		'translate(' + margin.left + ',' + margin.top + ')');
+
+// // Get the data
+// d3.csv('data2.csv', function (error, data) {
+// 	if (error) throw error;
+
+// 	// format the data
+// 	data.forEach(function (d) {
+// 		d.date = parseTime(d.date);
+// 		d.close = +d.close;
+// 		d.open = +d.open;
+// 	});
+
+// 	// Scale the range of the data
+// 	x.domain(d3.extent(data, function (d) {
+// 		return d.date;
+// 	}));
+// 	y.domain([0, d3.max(data, function (d) {
+// 		return Math.max(d.close, d.open);
+// 	})]);
+
+// 	// Add the valueline path.
+// 	svg.append('path')
+// 		.data([data])
+// 		.attr('class', 'line')
+// 		.attr('d', valueline);
+
+// 	// Add the valueline2 path.
+// 	svg.append('path')
+// 		.data([data])
+// 		.attr('class', 'line')
+// 		.style('stroke', 'red')
+// 		.attr('d', valueline2);
+
+// 	// Add the X Axis
+// 	svg.append('g')
+// 		.attr('transform', 'translate(0,' + height + ')')
+// 		.call(d3.axisBottom(x));
+
+// 	// Add the Y Axis
+// 	svg.append('g')
+// 		.call(d3.axisLeft(y));
+
+// });
