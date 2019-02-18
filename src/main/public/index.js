1e6ea1634ad95751376880f4e91bf29e08b23b09
Yarin Ronel <y3arin@gmail.com>
2019-02-19 00:14:36 +0200

    refactor: Extract shared code to generic function

--- src/main/public/index.js
+++ src/main/public/index.js
@@ -37,26 +37,26 @@ d3.json(mockFile, (error, data) => {
     genetic: element.geneticDuration
   }));
 
-  drawWorkersScoreGraph(workersScoreData);
-  drawWorkersDurationGraph(workersDurationData);
+  drawGraph(workersScoreData, "#workers-score");
+  drawGraph(workersDurationData, "#workers-duration");
 });
 
-function drawWorkersScoreGraph(workersScoreData) {
-  const data = workersScoreData;
-
+function drawGraph(data, selector) {
   // format the data
-  data.forEach(function(d) {
+  data.forEach(d => {
     d.workers = +d.workers;
     d.dynamic = +d.dynamic;
     d.genetic = +d.genetic;
   });
 
+  // Set diemensions
   const margin = { top: 20, right: 20, bottom: 30, left: 50 };
   const width = 960 - margin.left - margin.right;
   const height = 500 - margin.top - margin.bottom;
 
+  // Append graph element into DOM
   const svg = d3
-    .select("#workers-score")
+    .select(selector)
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -117,87 +117,6 @@ function drawWorkersScoreGraph(workersScoreData) {
     .append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x).tickFormat(d3.format("d")));
-
-  // Add the Y Axis
-  svg.append("g").call(d3.axisLeft(y));
-}
-
-function drawWorkersDurationGraph(workersDurationData) {
-  const data = workersDurationData;
-
-  // format the data
-  data.forEach(function(d) {
-    d.workers = +d.workers;
-    d.dynamic = +d.dynamic;
-    d.genetic = +d.genetic;
-  });
-
-  const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-  const width = 960 - margin.left - margin.right;
-  const height = 500 - margin.top - margin.bottom;
-
-  const svg = d3
-    .select("#workers-duration")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  // set the ranges
-  const x = d3.scaleLinear().range([0, width]);
-  const y = d3.scaleLinear().range([height, 0]);
-
-  // define the lines
-  var genetic = d3
-    .line()
-    .x(function(d) {
-      return x(d.workers);
-    })
-    .y(function(d) {
-      return y(d.genetic);
-    });
-
-  var dynamic = d3
-    .line()
-    .x(function(d) {
-      return x(d.workers);
-    })
-    .y(function(d) {
-      return y(d.dynamic);
-    });
-
-  // Scale the range of the data
-  x.domain(
-    d3.extent(data, function(d) {
-      return d.workers;
-    })
-  );
-  y.domain([
-    0,
-    d3.max(data, function(d) {
-      return Math.max(d.dynamic, d.genetic);
-    })
-  ]);
-
-  // Add paths
-  svg
-    .append("path")
-    .data([data])
-    .attr("class", "dynamic-line")
-    .attr("d", dynamic);
-
-  svg
-    .append("path")
-    .data([data])
-    .attr("class", "genetic-line")
-    .attr("d", genetic);
-
-  // Add the X Axis
-  svg
-    .append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
 
   // Add the Y Axis
   svg.append("g").call(d3.axisLeft(y));
